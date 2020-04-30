bead75a7959d14de270e1b34910f02286ccc2bbb
Stefano <stefano2890@gmail.com>
2020-04-30 17:17:38 +0200

    backwards and forwards and loop done

--- main.js
+++ main.js
@@ -5,8 +5,34 @@ $('.forwards').click(function(){
     var img_current = $('img.active');
     // tolgo la classe con il display block;
     img_current.removeClass('active');
-    // selezione l'immagine successiova con il .next;
+    // selezione l'immagine successiva con il .next;
     var img_next = img_current.next('img');
+    // creo la condizione per il loop;
+    // lenght sta per "c'è un'immagine successiva?". Se la risposta è sì allora il suo valore sarà pari a 1 altrimenti sarà pari a 0 (true or false)
+    if(img_next.length){
     // aggiungo la classe con il display block all'immagine successiva;
     img_next.addClass('active');
+    // se falso aggiungo la classe active alla prima immagine e quindi torno direttamente all'inizio del carosello;
+    } else {
+        img_next = $('img:first-of-type').addClass('active');
+    }
+})
+
+// replico la stessa funzione per lo scorrimento a sinistra;
+$('.backwards').click(function(){
+    // definisco l'immagine corrente;
+    var img_current = $('img.active');
+    // tolgo la classe con il display block;
+    img_current.removeClass('active');
+    // selezione l'immagine successiva con il .next;
+    var img_next = img_current.prev('img');
+    // creo la condizione per il loop;
+    // lenght sta per "c'è un'immagine successiva?". Se la risposta è sì allora il suo valore sarà pari a 1 altrimenti sarà pari a 0 (true or false)
+    if(img_next.length){
+    // aggiungo la classe con il display block all'immagine successiva;
+    img_next.addClass('active');
+    // se falso aggiungo la classe active alla prima immagine e quindi torno direttamente all'inizio del carosello;
+    } else {
+        img_next = $('img:last-of-type').addClass('active');
+    }
 })
