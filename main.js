04fbebe9f2dabc0c8560cf59c5fab40052b1d8bf
Stefano <stefano2890@gmail.com>
2020-05-04 16:36:59 +0200

    aggiunto timer per auto slide.

--- main.js
+++ main.js
@@ -106,3 +106,40 @@ $('.pallini i:eq(4)').click(function(){
     $('.pallini i:eq(4)').addClass('fas')
 
 })
+
+
+// Fare sì che il carousel funzioni con l'autoplay, ossia che automaticamente ogni 3 secondi cambi slide e venga visualizzata l'immagine successiva;
+// Dichiaro una variabile e imposto l'intervallo di tempo. Inserisco poi la funzione con le istruzioni per lo slide dell'immagine;
+var clock = setInterval(auto_slide, 3000);
+function auto_slide() {
+    // definisco l'immagine corrente;
+    var img_current = $('img.active');
+    // definisco pallino corrente;
+    var circle_current = $('.fas.fa-circle');
+    // tolgo la classe con il display block;
+    img_current.removeClass('active');
+    // tolgo la classe che rende il pallino pieno;
+    circle_current.removeClass('fas');
+    // cambio la classe con quella del pallino vuoto;
+    circle_current.addClass('far');
+    // selezione l'immagine successiva con il .next;
+    var img_next = img_current.next('img');
+    // selezione pallino successivo;
+    var circle_next = circle_current.next('.pallini i');
+    // creo la condizione per il loop;
+    // lenght sta per "c'è un'immagine successiva?". Se la risposta è sì allora il suo valore sarà pari a 1 altrimenti sarà pari a 0 (true or false)
+    if(img_next.length){
+    // aggiungo la classe con il display block all'immagine successiva;
+    img_next.addClass('active');
+    // inverto le classi dei pallini per sotituire da vuoto a pieno;
+    circle_next.removeClass('far');
+    circle_next.addClass('fas')
+    // se falso aggiungo la classe active alla prima immagine e quindi torno direttamente all'inizio del carosello;
+    } else {
+        img_next = $('img:first-of-type').addClass('active');
+        // per i pallini la condizione false significa in egual modo ripartire dall'inizio e ripristinare quindi la condizione iniziale, ossia con il primo pallino pieno e tutti i successivi vuoti;
+        circle_next = $('.pallini i:first-of-type').removeClass('far');
+        circle_next = $('.pallini i:first-of-type').addClass('fas')
+    }
+
+}
